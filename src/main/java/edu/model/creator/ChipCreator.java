eae586c396f5b032e79b3dd6f8dc5a7f0144762f
teatro <teatrocamillo@gmail.com>
2024-08-27 13:13:48 +0200

    updated ChipCreator

--- src/main/java/edu/model/creator/ChipCreator.java
+++ src/main/java/edu/model/creator/ChipCreator.java
@@ -21,6 +21,16 @@ public class ChipCreator extends Creator{
 		chipCodeMap.put(7404, new Chip7404());
 		chipCodeMap.put(7408, new Chip7408());
 		chipCodeMap.put(7410, new Chip7410());
+		chipCodeMap.put(7411, new Chip7411());
+		chipCodeMap.put(7420, new Chip7420());
+		chipCodeMap.put(7431, new Chip7431());
+		chipCodeMap.put(7432, new Chip7432());
+		chipCodeMap.put(7434, new Chip7434());
+		chipCodeMap.put(7442, new Chip7442());
+		chipCodeMap.put(7444, new Chip7444());
+		chipCodeMap.put(7482, new Chip7482());
+		chipCodeMap.put(74138, new Chip74138());
+		chipCodeMap.put(74152, new Chip74152());
 
 		this.uniqueChipIdGenerator = 0;
 	}
